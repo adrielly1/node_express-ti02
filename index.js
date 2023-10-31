57077ba57a5fbe4674ef69a97b61a9e203bec967
EA - Adrielly Camargo Dos Santos Alicate <adrielly_camargo@educadventista.org>
2023-10-31 14:18:52 -0300

    Recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -3,10 +3,37 @@ const path = require("path")
 
 const app = express()
 
-const caminhoBase = path.join(__dirname, "templates") >
+const caminhoBase = path.join(__dirname, "templates")
 
-app.get('/', (requisicao, resposta) => {
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuário é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+app.get('/', (requisicao, resposta) =>{
     resposta.sendFile(`${caminhoBase}/index.html`)
+
 })
 
 app.listen(3000, () => {
